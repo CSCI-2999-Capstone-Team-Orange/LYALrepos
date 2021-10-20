b38a25bf7dd17847a6cf7a4e7b42ce1457d1c10f
Katherine Graham <kgraham48@student.cscc.edu>
2021-10-20 15:25:08 -0400

    scaffold

---
+++ LoveYouALatte.Data/Entities/Inventory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace LoveYouALatte.Data.Entities
+{
+    public partial class Inventory
+    {
+        public int InventoryId { get; set; }
+        public string InvName { get; set; }
+        public string InvSize { get; set; }
+        public int? InvQuantity { get; set; }
+        public decimal? InvPrice { get; set; }
+        public string InvDescription { get; set; }
+    }
+}
